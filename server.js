57a5be8323c73481b827082aef58f20f5b8d2a88
RonitKumar09 <ronitkumar.robot@gmail.com>
2021-10-22 16:31:51 +0530

    Add CORS

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const authRoute = require("./Routes/authRoute/auth.js");
 const feedRoute = require("./Routes/feedRoutes/feed");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
-
+app.use(cors());
 app.use(express.json());
 
 //Database Connection
@@ -30,6 +31,10 @@ app.get("/", (req, res) => {
 //route middleware
 app.use("/auth", authRoute);
 app.use("/discover", feedRoute);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Page Not Foun" });
+});
 
 app.listen(8080, () => {
   console.log("server running at PORT 8080");
